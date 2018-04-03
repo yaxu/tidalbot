3bbf13adf9ea1f464f30014bee72b6ec64a0ca90
alex <alex@slab.org>
2018-04-03 22:17:57 +0000

    twiddles

--- startsc.sh
+++ startsc.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
+HOME=/home/tidal
+export HOME
+PATH="/usr/local/bin:/usr/bin:/bin:/snap/bin"
+export PATH
 startsc() {
    killall scsynth
    killall sclang
    tmux kill-session -t supercollider
    tmux new-session -s supercollider -d /usr/local/bin/sclang
-   if [[ $? -eq 0 ]]; then
-     echo tmux started
-   else
-     echo An error occurred starting tmux. RC was $?
-   fi
+   #if [[ $? -eq 0 ]]; then
+   #  echo tmux started
+   #else
+   #  echo An error occurred starting tmux. RC was $?
+   #fi
 }
 /bin/pidof scsynth
 if [[ $? -ne 0 ]]; then
-  echo Creating scsynth
+  #echo Creating scsynth
   startsc
 fi
